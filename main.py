f80e8e69abe59248858d3b79e81f166fc486b6d0
Dharmanshu Saini <dharmanshu39@gmail.com>
2018-04-30 14:03:50 +0530

    The Bot Remembers

--- main.py
+++ main.py
@@ -3,8 +3,19 @@
 import praw
 import requests
 import time
-import getpass
 from bs4 import BeautifulSoup
+import configparser
+import json
+
+# Getting Comment History
+
+dfile = 'data.txt'
+history = {}
+try:
+    data = open(dfile, 'r')
+    history = json.load(data)
+except Exception:
+    pass
 
 
 def getVideoLinkTitle(url):
@@ -32,11 +43,12 @@ def comment(reddit):
 
     ^^Link ^^for ^^who ^^can't ^^access ^^Streamable
 
-    I'm a bot :)"""
+    ^^I'm ^^a ^^bot ^^:)"""
         for submission in subreddit.new(limit=50):
             url = submission.url
             if url.find("streamable.com/") != -1:
                 if str(submission.title) not in history:
+                    history[str(submission.title)] = True
                     history[str(submission.title)] = 1
                     print("Working On", url)
                     link, title = getVideoLinkTitle(url)
@@ -45,17 +57,21 @@ def comment(reddit):
     return history
 
 
+# Getting data from user.client_id
+config = configparser.ConfigParser()
+config.read('user.ini')
 # Authentication
-client_id = input("client_id ")
-client_secret = input("client_secret ")
-password = getpass.getpass()
-username = input("username ")
-user_agent = "StreamableDOTBot 1.0"
+client_id = config.get('credentials', 'client_id')
+client_secret = config.get('credentials', 'client_secret')
+password = config.get('credentials', 'password')
+username = config.get('credentials', 'username')
+user_agent = config.get('credentials', 'user_agent')
 reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password=password, user_agent=username, username=username)
 print("Authentication Successfull")
-history = {}
 # getting new posts
 while True:
     print("Running")
     history = comment(reddit)
-    time.sleep(30)
+    with open(dfile, 'w') as outfile:
+        json.dump(history, outfile)
+    time.sleep(15)
